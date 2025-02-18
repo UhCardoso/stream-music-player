58d7d47a9b6836312a4fce88c04b24ad42890abb
werlensantos <werlencarsts@gmail.com>
2025-02-18 10:45:33 -0300

    ajustes para o deploy

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -2,7 +2,8 @@ import 'dotenv/config';
 import axios from "axios";
 
 const NODE_ENV = process.env.NODE_ENV;
-const URL = NODE_ENV === 'development' ? "http://localhost:3000/api" : '/api';
+//const URL = NODE_ENV === 'development' ? "http://localhost:3000/api" : '/api';
+const URL = 'http://stream-music-player.onrender.com/api'
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
