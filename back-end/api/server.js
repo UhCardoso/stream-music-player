34660651fe392ecc50a4cb46950faa6ebdaa0716
werlensantos <werlencarsts@gmail.com>
2025-02-18 10:30:03 -0300

    ajustes para o deploy

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -2,22 +2,32 @@ import express from "express";
 import cors from "cors";
 import { db } from "./connect.js";
 
+import path from 'path';
+
+const __dirname = path.resolve();
+
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/api/", (req, res) => {
   return res.send("Só vamos trabalhar com os endpoints '/artists' e '/songs'");
 });
 
-app.get("/artists", async (req, res) => {
+app.get("/api/artists", async (req, res) => {
   return res.send(await db.collection("artists").find({}).toArray());
 });
 
-app.get("/songs", async (req, res) => {
+app.get("/api/songs", async (req, res) => {
   return res.send(await db.collection("songs").find({}).toArray());
+});
+
+app.use(express.static(path.join(__dirname, '../../front-end/dist')));
+
+app.get("*", async (req, res) => {
+  return res.sendFile(path.join(__dirname, '../../front-end/dist/index.html'));
 });
 
 app.listen(PORT, () => {
